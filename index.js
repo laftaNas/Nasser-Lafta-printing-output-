3596d10e59d2269435c0fdcb71fb2bf08c2697bb
laftaNas <77341443+laftaNas@users.noreply.github.com>
2021-01-22 12:45:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+//TASK ONE
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = '';
+output += 'let a = 3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c;';
+output += '\n';
+output += '----------';
+output += '\n';
+output += 'a + b = ' + (a + b);
+output += '\n';
+output += 'a - b = ' + (a - b);
+output += '\n';
+output += 'a * b = ' + (a * b);
+output += '\n';
+output += 'a / b = ' + (a / b);
+output += '\n';
+output += 'a % b = ' + (a % b);
+output += '\n';
+output += '(a += b): ' + (a += b);
+output += '\n';
+output += '(a -= b): ' + (a -= b);
+output += '\n';
+output += '(a *= b): ' + (a *= b);
+output += '\n';
+output += '(a /= b): ' + (a /= b);
+output += '\n';
+output += '(a == b): ' + (a == b);
+output += '\n';
+output += '(a != b): ' + (a != b);
+output += '\n';
+output += '(a > b): ' + (a > b);
+output += '\n';
+output += '(a < b): ' + (a < b);
+output += '\n';
+output += '(!a && !c): ' + (!a && !c);
+output += '\n';
+output += '(!a || !c): ' + (!a || !c);
+
+
+alert(output);
+
+let output2 = '';
+
+let first_name = 'Nasser';
+output2 += 'My name is ' + first_name;
+output2 += '\n';
+
+let last_name = 'Lafta';
+output2 += 'My last name is ' + last_name;
+output2 += '\n';
+
+let email = 'laft0009@algonquinlive.com';
+output2 += 'My email is ' + email;
+
+output2 += '\n';
+
+let quote = 'What is normal for the spider is chaos for the fly — Morticia Addams';
+output2 += 'My quote is ' + quote;
+
+output2 += '\n';
+
+alert(output2);
+
